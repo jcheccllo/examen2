a7372a3c60dd721e1db8c85ec6ee581ee60cc195
jose e <jegusquiza@gmail.com>
2013-04-08 22:02:06 -0500

    examen

---
+++ RESTFulDemo/IRESTService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RESTFulDemo
+{
+
+    #region IRESTService Interface
+
+    [ServiceContract]
+    public interface IRestSerivce
+    {
+        //2) aqui colocamos los metodos de la clase
+        //POST operation
+        //web invoke sirve para, un create siempre es invoke
+        [OperationContract]
+        [WebInvoke(UriTemplate = "", Method = "POST")]
+        Student CreateStudent(Student createStudent);
+
+
+        //Get Operation
+        //web get sirve para el url base, si no te pasa nada te manda todos
+        [OperationContract]
+        [WebGet(UriTemplate = "")]
+        List<Student> GetAllStudent();
+        //si te manda id, solo muestra la persona con ese id
+        [OperationContract]
+        [WebGet(UriTemplate = "{id}")]
+        Student GetAStudent(string id);
+
+        //PUT Operation
+        // el operation contract indica que es metodo, el web invoke  es despues
+        //par ael put si se le manda el id y el metodo es put obviamente
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
+        Student UpdateStudent(string id, Student updateStudent);
+
+        //DELETE Operation
+        // se manda id pero solamente delete
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{id}", Method = "DELETE")]
+        void DeleteStudent(string id);
+
+
+       }
+    
+    #endregion
+
+
+    // 1) lo primer q hacemos es esta parte la clase person
+    #region Student Entity
+
+    //Para definir entidad data conctract
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public string ID;
+        [DataMember]
+        public string Name;
+        [DataMember]
+        public string Lastname;
+        [DataMember]
+        public string Age;
+        
+
+    } 
+    #endregion
+
+}
---
+++ RESTFulDemo/RESTService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Activation;
+using System.ServiceModel;
+
+namespace RESTFulDemo
+{
+    /// <summary>
+    /// Basically this code is developed for HTTP GET, PUT, POST & DELETE operation.
+    /// </summary>
+    /// 
+    //estas dos lineas nos sirven para la compatibilidad de asp.net
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+
+    //implementamps la interface, los dos puntos es como el implements de java
+    // para rellenar con los metdos de la interface , simpleente damos a rellenar con los metodos cuando la I del IRESTSERVICE sea desplegable
+    public class RESTSerivce : IRestSerivce
+    {
+        //ponemos las variables a usar por la clase
+        List<Student> objStudent = new List<Student>();
+        int studentCount = 0;
+
+        public Student CreateStudent(Student createStudent)
+        {
+            //desarrollamos el metodo de creacion de perosna
+            createStudent.ID = (++studentCount).ToString();
+            objStudent.Add(createStudent);
+            return createStudent;
+        }
+
+        public List<Student> GetAllStudent()
+        {   
+            //quitamos el trown y ponemos el return toda la lista
+            return objStudent.ToList();
+        }
+
+        public Student GetAStudent(string id)
+        {
+            //desarrollamos el buscar perosna con id tal
+            return objStudent.FirstOrDefault(e => e.ID.Equals(id));
+        }
+
+        public Student UpdateStudent(string id, Student updateStudent)
+        {
+            //desarrollamos el actualizar perosna mandamos el id y losdatos de la persona a actualizar
+            Student s = objStudent.FirstOrDefault(e => e.ID.Equals(id));
+            s.Name = updateStudent.Name;
+            s.Name = updateStudent.Lastname;
+            s.Age = updateStudent.Age;
+            return s;
+        }
+
+        public void DeleteStudent(string id)
+        {
+            //desarollamos el delete
+            objStudent.RemoveAll(e => e.ID.Equals(id));
+        }
+
+      
+    }
+}
